a426ffc32c46b1849c4abd85d770c3bf5f435770
Alexandre Marra <alexsmarra@hotmail.com>
2025-07-18 14:20:10 -0300

    Entendendo o TypeScript update

--- src/index.js
+++
@@ -1,8 +0,0 @@
-// nos parâmetros, está dizendo que user tem que seguir a regra da interface User
-function calculateAgeOfUser(user) {
-    return new Date().getFullYear() - user.birthYear;
-}
-calculateAgeOfUser({
-    birthYear: 1976
-});
-console.log("adfaf");
