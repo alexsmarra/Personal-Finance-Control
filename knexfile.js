583ec8c79233790fd9c087fed30a8009fa1b350e
Alexandre Marra <alexsmarra@hotmail.com>
2025-08-13 11:00:27 -0300

    update README.md and delete knexfile.js file

--- knexfile.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-// importar as configurações
-const database_1 = require("./src/database");
-// exportar as configurações
-exports.default = database_1.config;
